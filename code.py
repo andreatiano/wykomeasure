14af59de9e93a2f73e8fd13e2051bb0f635a039e
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-02-11 20:12:55 +0100

    Update code.py

--- code.py
+++ code.py
@@ -8,6 +8,7 @@ importExp=st.sidebar.expander('Import Option')
 Delimiter= importExp.selectbox('Delimiter:',('\t',';',','))
 spectra = importExp.file_uploader("upload file", type={"csv", "txt"})
 dataset=[]
+list=[]
 if spectra is not None:
     spectra_df = pd.read_csv(spectra)
     st.write(spectra_df)
@@ -18,12 +19,15 @@ if spectra is not None:
     if column and rows is not None:
         columnNumber=spectra_df.columns.get_loc(column[0]) 
         allWafer = importExp.file_uploader("after rows and column selection import all files", type={"csv", "txt"},accept_multiple_files=True)
-        if allWafer is not None:      
+        if allWafer is not None:  
+            i=0
             for uploaded_file in allWafer:
+               i++
                spectrafor_df = pd.read_csv(uploaded_file,skiprows=rows[0])
+               list.append(f'Wafer_{i}')
                waferdata=spectrafor_df.iloc[:,columnNumber]
                dataset.append(waferdata)
                finalDataset=np.array(dataset)
             plotData=st.expander('fitted data',True)
-            plotDataFrame=pd.DataFrame(finalDataset.transpose())
+            plotDataFrame=pd.DataFrame(finalDataset.transpose(),columns=list)
             plotData.dataframe(plotDataFrame) 
