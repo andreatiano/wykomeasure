39e4fcd3f75dac7df609563b13953e2a0373b7f3
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-02-11 20:52:25 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,34 +1,37 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import streamlit as st
+ import matplotlib.pyplot as plt
+ import pandas as pd
+ import streamlit as st
+ import plotly.graph_objects as go
 
-st.set_page_config(layout="wide")
-plotCol, dataCol = st.columns([2,1])
-importExp=st.sidebar.expander('Import Option')
-Delimiter= importExp.selectbox('Delimiter:',('\t',';',','))
-allWafer = importExp.file_uploader("Import measurement files", type={"csv", "txt"},accept_multiple_files=True)
-dataset=[]
-list=[]
-righe=[]
-if allWafer is not None:
-    layout_df = pd.read_csv(allWafer[1])
-    rows = st.multiselect(
-    'select the row of the first measure',layout_df.index) 
-    column = st.multiselect(
-    'select the correct column',layout_df.columns.values.tolist())  
-    st.write(layout_df)
-    if column and rows is not None:
-            columnNumber=layout_df.columns.get_loc(column[0])  
-            i=0
-            for uploaded_file in allWafer:
-               i=i+1
-               spectrafor_df = pd.read_csv(uploaded_file,skiprows=rows[0])
-               list.append(f'Wafer_{i}')
-               waferdata=spectrafor_df.iloc[:,columnNumber]
-               dataset.append(waferdata)
-               finalDataset=np.array(dataset)
-            plotData=st.expander('Final Dataset')
-            plotDataFrame=pd.DataFrame(finalDataset.transpose(),columns=list)
-            plotData.dataframe(plotDataFrame)
-            plotData.download_button('Download current Dataset',plotDataFrame.to_csv().encode('utf-8'),'Measure.csv')
+ st.set_page_config(layout="wide")
+ plotCol, dataCol = st.columns([2,1])
+ importExp=st.sidebar.expander('Import Option')
+ Delimiter= importExp.selectbox('Delimiter:',('\t',';',','))
+ spectra = importExp.file_uploader("upload file", type={"csv", "txt"})
+ list=[]
+ dataset=[]
+ if spectra is not None:
+     spectra_df = pd.read_csv(spectra)
+     st.write(spectra_df)
+     rows = st.multiselect(
+     'select the row of the first measure',spectra_df.index) 
+     column = st.multiselect(
+     'select the correct column',spectra_df.columns.values.tolist())  
+     st.write(spectra_df)
+     if column and rows is not None:
+             columnNumber=spectra_df.columns.get_loc(column[0]) 
+             allWafer = importExp.file_uploader("After rows and column selection import all files", type={"csv", "txt"},accept_multiple_files=True)
+             i=0
+             for uploaded_file in allWafer:
+                i=i+1
+                spectrafor_df = pd.read_csv(uploaded_file,skiprows=rows[0])
+                list.append(f'Wafer_{i}')
+                waferdata=spectrafor_df.iloc[:,columnNumber]
+                dataset.append(waferdata)
+                finalDataset=np.array(dataset)
+             plotData=st.expander('Final Dataset',True)
+             plotDataFrame=pd.DataFrame(finalDataset.transpose(),columns=list)
+             plotData=dataCol.expander('Final Dataset')
+             plotDataFrame=pd.DataFrame(finalDataset.transpose(),columns=list)
+             plotData.dataframe(plotDataFrame)
+             plotData.download_button('Download current Dataset',plotDataFrame.to_csv().encode('utf-8'),'Measure.csv')
