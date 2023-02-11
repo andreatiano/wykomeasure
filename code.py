cd8934ee6c84fa9547745def75dfb0a4b2db3101
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-02-11 19:40:57 +0100

    Update code.py

--- code.py
+++ code.py
@@ -16,14 +16,15 @@ if spectra is not None:
     # spectra_df1=pd.read_csv(spectra,skiprows=rows-1)
     column = st.multiselect(
     'select the correct column',spectra_df.columns.values.tolist())  
-    allWafer = importExp.file_uploader("after rows and column selection import all files", type={"csv", "txt"},accept_multiple_files=True)
     if column and rows is not None:
-            columnNumber=spectra_df.columns.get_loc(column[0]) 
+        columnNumber=spectra_df.columns.get_loc(column[0]) 
+        allWafer = importExp.file_uploader("after rows and column selection import all files", type={"csv", "txt"},accept_multiple_files=True)
+        if allWafer is not None:      
             for uploaded_file in allWafer:
                spectrafor_df = pd.read_csv(uploaded_file,skiprows=rows[0]-1)
                waferdata=spectrafor_df.iloc[:,columnNumber]
                dataset.append(waferdata)
                finalDataset=np.array(dataset)
             plotData=st.expander('fitted data',True)
-            plotDataFrame=pd.DataFrame(dataset.transpose())
+            plotDataFrame=pd.DataFrame(finalDatasetdataset.transpose())
             plotData.dataframe(plotDataFrame) 
