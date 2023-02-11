4d56fa087c1fa60a06c7cc12559ac0900992fa2c
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-02-11 19:43:46 +0100

    Update code.py

--- code.py
+++ code.py
@@ -13,7 +13,6 @@ if spectra is not None:
     st.write(spectra_df)
     rows = st.multiselect(
     'select the row of the first measure',spectra_df.index) 
-    # spectra_df1=pd.read_csv(spectra,skiprows=rows-1)
     column = st.multiselect(
     'select the correct column',spectra_df.columns.values.tolist())  
     if column and rows is not None:
@@ -21,7 +20,7 @@ if spectra is not None:
         allWafer = importExp.file_uploader("after rows and column selection import all files", type={"csv", "txt"},accept_multiple_files=True)
         if allWafer is not None:      
             for uploaded_file in allWafer:
-               spectrafor_df = pd.read_csv(uploaded_file,skiprows=rows[0]-1)
+               spectrafor_df = pd.read_csv(uploaded_file,skiprows=rows[0]+1)
                waferdata=spectrafor_df.iloc[:,columnNumber]
                dataset.append(waferdata)
                finalDataset=np.array(dataset)
