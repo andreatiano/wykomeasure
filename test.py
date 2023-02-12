9f2434e75bdb6b51a615fe6855da70e8a52cbce8
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-02-12 12:33:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -21,14 +21,14 @@ if Data is not None:
              columnNumber=Data_df.columns.get_loc(column[0])  
              waferdata1=Data_df.iloc[rows[0]:,columnNumber]
              dataset.append(waferdata1)
+             list.append('Wafer_1')
              for l in range (1,len(Data)):
                 Datafor_df = pd.read_csv(Data[l],skiprows=rows[0])
-                list.append('Wafer_1')
                 list.append(f'Wafer_{l+1}')
                 waferdata=Datafor_df.iloc[:,columnNumber]
                 dataset.append(waferdata)
              finalDataset=np.array(dataset)
              plotData=dataCol.expander('Final Dataset',True)
-             plotDataFrame=pd.DataFrame(finalDataset.transpose())
+             plotDataFrame=pd.DataFrame(finalDataset.transpose(),columns=list)
              plotData.dataframe(plotDataFrame)
              plotData.download_button('Download current Dataset',plotDataFrame.to_csv().encode('utf-8'),'Measure.csv')
