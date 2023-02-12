a336a5772b766d82345abcf04b561d0c946c36e9
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-02-12 12:37:42 +0100

    Update code.py

--- code.py
+++ code.py
@@ -7,30 +7,28 @@ st.set_page_config(layout="wide")
 plotCol, dataCol = st.columns([2,1])
 importExp=st.sidebar.expander('Import Option')
 Delimiter= importExp.selectbox('Delimiter:',('\t',';',','))
-spectra = importExp.file_uploader("upload file")
+Data= importExp.file_uploader("upload file",accept_multiple_files=True)
 list=[]
 dataset=[]
-if spectra is not None:
-     spectra_df = pd.read_csv(spectra)
+Data_df = pd.read_csv(Data[0])
+if Data is not None:
      rows = plotCol.multiselect(
-     'select the row of the first measure',spectra_df.index) 
+     'select the row of the first measure',Data_df.index) 
      column = plotCol.multiselect(
-     'select the correct column',spectra_df.columns.values.tolist())  
-     plotCol.write(spectra_df)
+     'select the correct column',Data_df.columns.values.tolist())  
+     plotCol.write(Data_df) 
      if column and rows is not None:
-             columnNumber=spectra_df.columns.get_loc(column[0]) 
-             allWafer = importExp.file_uploader("After rows and column selection import all files",accept_multiple_files=True)
-             i=0
-             for uploaded_file in allWafer:
-                i=i+1
-                spectrafor_df = pd.read_csv(uploaded_file,skiprows=rows[0])
-                list.append(f'Wafer_{i}')
-                waferdata=spectrafor_df.iloc[:,columnNumber]
+             columnNumber=Data_df.columns.get_loc(column[0])  
+             waferdata1=Data_df.iloc[rows[0]:,columnNumber]
+             dataset.append(waferdata1)
+             list.append('Wafer_1')
+             for l in range (1,len(Data)):
+                Datafor_df = pd.read_csv(Data[l],skiprows=rows[0])
+                list.append(f'Wafer_{l+1}')
+                waferdata=Datafor_df.iloc[:,columnNumber]
                 dataset.append(waferdata)
-                finalDataset=np.array(dataset)
-             plotData=st.expander('Final Dataset',True)
-             plotDataFrame=pd.DataFrame(finalDataset.transpose(),columns=list)
-             plotData=dataCol.expander('Final Dataset')
+             finalDataset=np.array(dataset)
+             plotData=dataCol.expander('Final Dataset',True)
              plotDataFrame=pd.DataFrame(finalDataset.transpose(),columns=list)
              plotData.dataframe(plotDataFrame)
              plotData.download_button('Download current Dataset',plotDataFrame.to_csv().encode('utf-8'),'Measure.csv')
