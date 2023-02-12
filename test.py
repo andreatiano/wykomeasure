33c3555ce9e0f4463aafa5a8c10cf5278d3a8ed9
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-02-12 11:47:10 +0100

    Update test.py

--- test.py
+++ test.py
@@ -24,4 +24,5 @@ if spectra_df is not None:
              st.write(len(spectra))
              spectrafor_df = pd.read_csv(spectra[1])
              st.write(spectra)
+             st.write(spectrafor_df)
              
