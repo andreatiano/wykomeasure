365c23edcb689d2f13ca7bb9d41a7e9e3a842fa3
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-02-12 11:42:01 +0100

    Update test.py

--- test.py
+++ test.py
@@ -22,5 +22,6 @@ if spectra_df is not None:
              columnNumber=spectra_df.columns.get_loc(column[0]) 
              i=0
              st.write(len(spectra))
-             spectrafor_df = pd.read_csv(spectra[1])
+             spectrafor_df = pd.read_csv(spectra[2])
+             st.write(spectrafor_df)
              
