8cd2f48a561a8b8d688cd1ea39ab14b5bd2fbaba
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-02-28 10:50:41 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,16 +2,19 @@ import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
 import numpy as np
-
+import parse
 st.set_page_config(layout="wide")
 plotCol, dataCol = st.columns([2,1])
 importExp=st.sidebar.expander('Import Option')
+importExp.subheader('After import press the button')
+verified=importExp.checkbox('Press to start')
 Delimiter= importExp.selectbox('Delimiter:',('\t',';',','))
 Data= importExp.file_uploader("upload file",accept_multiple_files=True)
+
 list=[]
 dataset=[]
-Data_df = pd.read_csv(Data[0])
-if Data is not None:
+if verified:
+     Data_df = pd.read_csv(Data[0])
      rows = plotCol.multiselect(
      'select the row of the first measure',Data_df.index) 
      column = plotCol.multiselect(
@@ -21,10 +24,12 @@ if Data is not None:
              columnNumber=Data_df.columns.get_loc(column[0])  
              waferdata1=Data_df.iloc[rows[0]:,columnNumber]
              dataset.append(waferdata1)
-             list.append('Wafer_1')
+             result1 = parse.search('CarrierAtPort1.{}_', Data[0].name)
+             list.append(f'Wafer_{result1.fixed}')
              for l in range (1,len(Data)):
                 Datafor_df = pd.read_csv(Data[l],skiprows=rows[0])
-                list.append(f'Wafer_{l+1}')
+                result = parse.search('CarrierAtPort1.{}_', Data[l].name)
+                list.append(f'Wafer_{result.fixed}')
                 waferdata=Datafor_df.iloc[:,columnNumber]
                 dataset.append(waferdata)
              finalDataset=np.array(dataset)
