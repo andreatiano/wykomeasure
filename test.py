529598c01c3b95b1297fb4930c97994104caa184
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-03-02 11:44:55 +0100

    Update test.py

--- test.py
+++ test.py
@@ -30,8 +30,8 @@ if verified:
                 Datafor_df = pd.read_csv(Data[l],skiprows=rows[0])
                 result = parse.search('CarrierAtPort1.{}.', Data[l].name)
                 list.append(f'Wafer_{result.fixed}'*leng)
-                
-                waferdata=Datafor_df.iloc[:,columnNumber]
+                list=np.array(list).T
+                waferdata'Datafor_df.iloc[:,columnNumber]
                 dataset.append(waferdata)
              finalDataset=np.array(dataset)
              finalDataset=finalDataset.reshape(1,len(waferdata1)*len(Data))
