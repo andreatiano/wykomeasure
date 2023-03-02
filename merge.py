6e906936016f001cf9a5ab038f4b479620975dfb
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-03-02 21:34:53 +0100

    Update merge.py

--- merge.py
+++ merge.py
@@ -11,6 +11,7 @@ verified=importExp.checkbox('Press to start')
 Delimiter= importExp.selectbox('Delimiter:',('\t',';',','))
 Data= importExp.file_uploader("upload file",accept_multiple_files=True)
 list=[]
+list2=[]
 if verified:
      Data_df = pd.read_csv(Data[0])
      rows = plotCol.multiselect(
@@ -22,14 +23,14 @@ if verified:
              columnNumber=Data_df.columns.get_loc(column[0])  
              dataset=Data_df.iloc[rows[0]:]
              result1 = parse.search('CarrierAtPort1.{}.', Data[0].name)
-             list =(f'Wafer_{result1.fixed}')
+             list.append(f'Wafer_{result1.fixed}')
              dimension=len(dataset)
              list=list*dimension
              for l in range (1,len(Data)):
                 Datafor_df = pd.read_csv(Data[l],skiprows=rows[0])
                 dataset.append(Datafor_df)
                 result = parse.search('CarrierAtPort1.{}.', Data[l].name)
-                list2=(f'Wafer_{result.fixed}')
+                list2.append(f'Wafer_{result.fixed}')
                 list2=list2*dimension
                 list.append(list2)
              finallist=pd.series(list)
