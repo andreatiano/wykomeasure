f530b130cb6187d4bdb1e7c3462a7932305f8325
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-03-02 21:05:53 +0100

    Update merge.py

--- merge.py
+++ merge.py
@@ -11,6 +11,7 @@ verified=importExp.checkbox('Press to start')
 Delimiter= importExp.selectbox('Delimiter:',('\t',';',','))
 Data= importExp.file_uploader("upload file",accept_multiple_files=True)
 list=[]
+listdata
 dataset=[]
 if verified:
      Data_df = pd.read_csv(Data[0])
@@ -21,18 +22,16 @@ if verified:
      plotCol.write(Data_df) 
      if column and rows is not None:
              columnNumber=Data_df.columns.get_loc(column[0])  
-             waferdata1=Data_df.iloc[rows[0]:,columnNumber]
+             waferdata1=Data_df.iloc[rows[0]:]
              dataset.append(waferdata1)
              result1 = parse.search('CarrierAtPort1.{}.', Data[0].name)
              list.append(f'Wafer_{result1.fixed}')
              for l in range (1,len(Data)):
                 Datafor_df = pd.read_csv(Data[l],skiprows=rows[0])
+                dataset.append(Datafor_df)
                 result = parse.search('CarrierAtPort1.{}.', Data[l].name)
                 list.append(f'Wafer_{result.fixed}')
-                waferdata=Datafor_df.iloc[:,columnNumber]
-                dataset.append(waferdata)
-             finalDataset=np.array(dataset)
+             finalDatase=dataset[column]
              plotData=dataCol.expander('Final Dataset',True)
-             plotDataFrame=pd.DataFrame(finalDataset.transpose(),columns=list)
-             plotData.dataframe(plotDataFrame)
+             plotData.dataframe(finalDatase)
              plotData.download_button('Download current Dataset',plotDataFrame.to_csv().encode('utf-8'),'Measure.csv')
