1fb323f36f8cb67700cd0133bd37b281332ca600
andreatiano <86158089+andreatiano@users.noreply.github.com>
2023-03-02 10:53:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ if verified:
                 waferdata=Datafor_df.iloc[:,columnNumber]
                 dataset.append(waferdata)
              finalDataset=np.array(dataset)
-             finalDataset=finalDataset.reshape(Llen(waferdata1))
+             finalDataset=finalDataset.reshape(len(waferdata1))
              plotData=dataCol.expander('Final Dataset',True)
              plotDataFrame=pd.DataFrame(finalDataset)
              plotData.dataframe(plotDataFrame)
